2a53dcac2b2dca6a50f9476dec220fbef899699c
Jinwoo Park <jinwoopark@Jinwooui-MacBookAir.local>
2021-03-03 16:17:49 +0900

    Initial commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,40 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            //영속
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
+
+            System.out.println("=====================");
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+}
